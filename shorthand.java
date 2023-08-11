5565353f95b06a8b1c273f07636bd9eb679642dd
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-08-11 21:50:18 +0530

    Create shorthand.java

---
+++ shorthand.java
@@ -0,0 +1,7 @@
+public class shorthand {
+    public static void main(String[] args) {
+        int x=10;
+        String z=(x>11)?"number bigger than 11":"number smaller than 11";
+        System.out.println(z);
+    }
+}
