076004687fc40c3dc17536a7b03843b133b928ed
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-08-11 22:00:11 +0530

    Create switchcases.java

---
+++ switchcases.java
@@ -0,0 +1,16 @@
+public class  switchcases{
+    public static void main(String[] args) {
+        int x=2;
+        switch(x){
+            case 1:
+            System.out.println("this is 1");
+            break;
+            case 6:
+            System.out.println("This is 6");
+            break;
+            default:
+            System.out.println("Ye to hona hi tha");
+            break;
+        }   
+    }
+}
