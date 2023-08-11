1373399fa4ef8abb99c5efec07245ff476773857
Anshumaan Singh <anshumaanborntolead@gmail.com>
2023-08-11 22:17:02 +0530

    Create foreachwalaloop.java

---
+++ foreachwalaloop.java
@@ -0,0 +1,12 @@
+public class foreachwalaloop {
+    public static void main(String[] args) {
+        String names[]={"Shubh", "Rinku", "Somya", "Anshu"};
+        for(String a:names){
+            System.out.println(a);
+        }  
+        int l=names.length;
+        for(int i=0;i<l;i++){
+            System.out.println(names[i]);
+        } 
+    }
+}
